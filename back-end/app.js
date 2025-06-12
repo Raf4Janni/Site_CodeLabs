2d1b62cb110b2d8e45f133eb244b07c95c8c24a9
Raf4Janni <rsjannini@gmail.com>
2025-06-12 14:05:40 -0300

    Atualização funcao adicionar

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,17 @@
 let tarefas=[]
+let id=0;
 const botaoAdicionar = document.getElementById("botaoAdicionar")
 botaoAdicionar.addEventListener("click", () =>{
     let descricao = document.getElementById("desc").value
     let data = document.getElementById("data").value
     let prioridade = parseInt(document.getElementById("pri").value)
     tarefas.push({
+        id,
         descricao,
         data,
         prioridade
     })
+    id++
     document.getElementById("desc").value=""
     document.getElementById("data").value=""
     document.getElementById("pri").value=""
