2a106cf9f341a6a30455a2b3184628f11c385862
ArthDias06 <arthurcastrodias@gmail.com>
2025-06-14 15:16:36 -0300

    inicio do cadastro

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,28 @@
+var submit = document.getElementById("envio");
+var form = document.getElementsByTagName("form");
+submit.addEventListener("click", ()=>{
+    var nome = document.getElementById("nome").value;
+    var senha = document.getElementById("senha").value;
+    var confSenha = document.getElementById("confSenha").value;
+    if(senha != confSenha){
+        alert("As senhas não conferem!");
+        location.href = "http://127.0.0.1:5500/front-end/html/cadastro.html"
+    }else if(nome == "" || senha == ""){
+        alert("Preencha todos os campos!");
+        location.href = "http://127.0.0.1:5500/front-end/html/cadastro.html"
+    }
+    else{
+        window.localStorage.setItem("nome", nome);
+        window.localStorage.setItem("senha", senha);
+        location.href = "http://127.0.0.1:5500/front-end/html/index.html"
+    }
+});
+
+
+
+
+
+
 let tarefas=[]
 let id=0;
 const botaoAdicionar = document.getElementById("botaoAdicionar")
