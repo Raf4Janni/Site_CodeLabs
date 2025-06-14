a2271a72cc4a73f548fbfdb2afa27c1d59850a02
ArthDias06 <arthurcastrodias@gmail.com>
2025-06-14 16:22:40 -0300

    cadstro parece certo

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,21 @@
 var submit = document.getElementById("envio");
-var form = document.getElementsByTagName("form");
-submit.addEventListener("click", ()=>{
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
     var nome = document.getElementById("nome").value;
     var senha = document.getElementById("senha").value;
     var confSenha = document.getElementById("confSenha").value;
     if(senha != confSenha){
         alert("As senhas não conferem!");
-        location.href = "http://127.0.0.1:5500/front-end/html/cadastro.html"
+        window.location.href = "cadastro.html"
     }else if(nome == "" || senha == ""){
         alert("Preencha todos os campos!");
-        location.href = "http://127.0.0.1:5500/front-end/html/cadastro.html"
+        window.location.href = "cadastro.html"
     }
     else{
         window.localStorage.setItem("nome", nome);
         window.localStorage.setItem("senha", senha);
-        location.href = "http://127.0.0.1:5500/front-end/html/index.html"
+        window.location.href = "index.html"
     }
 });
 
@@ -25,19 +26,16 @@ submit.addEventListener("click", ()=>{
 
 let tarefas=[]
 let id=0;
-
 const botaoAdicionar = document.getElementById("botaoAdicionar")
 botaoAdicionar.addEventListener("click", () =>{
     let descricao = document.getElementById("desc").value
     let data = document.getElementById("data").value
     let prioridade = parseInt(document.getElementById("pri").value)
-    let concluida = false
     tarefas.push({
         id,
         descricao,
         data,
-        prioridade,
-        concluida 
+        prioridade
     })
     id++
     document.getElementById("desc").value=""
